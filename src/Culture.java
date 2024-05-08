13e5a97ac8637a45c20632ae3bd0bc879ce18b50
Brecken Shafer <bshafer@Millikin.edu>
2024-05-08 14:00:21 -0500

    Added Culture Class

---
+++ src/Culture.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Culture {
+
+    static List<String> Culture = new ArrayList<>();
+
+    List<String> culture = new ArrayList<>();
+
+    {
+
+        culture.add("American");
+        culture.add("Chinese");
+        culture.add("Japanese");
+        culture.add("Russian");
+
+
+        String cultureFirst = Culture.getFirst();
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
